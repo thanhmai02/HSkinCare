dc453fdadaa8ca36c7edb02fba308cd27a5e73d2
linhle.070801@gmail.com <linhle.070801@gmail.com>
2023-12-29 09:31:22 +0700

    Cập nhật tổng quan

--- js/common.js
+++ js/common.js
@@ -17,11 +17,85 @@ function fixedMenu() {
     }
 }
 
+const showContent = () => {
+    $('body').addClass('showBody');
+}
+
+/* =================== || HANDLE MOBILE MENU || =================== */
+$('.wrapBtnCtrMenuHeader .btnCtrMenu').on('click', function (e) {
+    $('body').toggleClass('showMenu wrapHidden');
+    e.preventDefault();
+
+    if ($('.showContact').length) {
+        $('body').removeClass('showContact wrapHidden');
+    }
+});
+
+$('.wrap-bg-menu-mobile').on('click', function (e) {
+    $('body').removeClass('showMenu wrapHidden');
+    e.preventDefault();
+});
+
+$('.btnDropdownMenuSub').on('click', function (e) {
+    $(this).next('.wrapMenuHeader').slideToggle();
+    $(this).parent('.menuSub').toggleClass('activeMenu');
+
+    e.preventDefault();
+});
+
+
+/* =================== || LANGUAGE || =================== */
+$('.language-btn').on('click', function () {
+    $('body').removeClass('showMenu');
+    $('body').toggleClass('showLanguage');
+    $('.dropdown-languge').fadeToggle();
+
+    if ($('.showMenu').length) {
+        $('body').removeClass('wrapHidden showMenu');
+    }
+});
+
+$('body, html').on('click', function (e) {
+    var target = $(e.target);
+    if (
+        e.type == "focusin" ||
+        target.closest(this.element).length ||
+        target.closest(this.container).length ||
+        target.closest('.header-language').length
+    ) return;
+
+    if ($('body').hasClass('showLanguage')) {
+        $('body').removeClass('showLanguage');
+        $('.dropdown-languge').fadeOut();
+    }
+});
+
+/* =================== || Button Scroll Top || =================== */
+function showBtnScrollTop() {
+    if ($('.wrapBtnScrollTop').length) {
+        var windowScrollTop = $(window).scrollTop();
+
+        $("body").toggleClass("showScrollTop", windowScrollTop > 1)
+    }
+}
+
+$(".wrapBtnScrollTop .linkItem").on('click', function (e) {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 100);
+
+    e.preventDefault();
+});
+
 /* =================== || INITIAL || =================== */
 $(window).on('load', function () {
     fixedMenu();
+    showBtnScrollTop();
+
+    showContent();
 });
 
 $(window).on('scroll', function () {
     fixedMenu();
+    showBtnScrollTop();
 });
